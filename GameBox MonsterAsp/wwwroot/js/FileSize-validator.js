d2dafbca2ca22575aea86f009cfc526755a0a55a
Hesham Fathy <heshamfathykamal@gmail.com>
2024-11-15 00:10:41 +0200

    delete temp data

--- GameBox MonsterAsp/wwwroot/js/FileSize-validator.js
+++
@@ -1,5 +0,0 @@
-//  add client side validation for file size
-
-$.validator.addMethod('filesize', function (value, element, param) {
-    return this.optional(element) || element.files[0].size <= param;
-});
